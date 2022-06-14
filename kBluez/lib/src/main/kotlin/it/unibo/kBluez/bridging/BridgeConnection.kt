4ca9c0796bbd33938174c4d214a6abdce59943d6
Luca Marchegiani <luca.marchegiani.96@gmail.com>
2022-06-14 16:37:24 +0200

    unibolibs added

--- kBluez/lib/src/main/kotlin/it/unibo/kBluez/bridging/BridgeConnection.kt
+++ kBluez/lib/src/main/kotlin/it/unibo/kBluez/bridging/BridgeConnection.kt
@@ -6,43 +6,69 @@ import it.unibo.kBluez.socket.BluetoothSocket
 import kotlinx.coroutines.*
 import mu.KotlinLogging
 import java.io.Closeable
+import java.nio.ByteBuffer
 
 class BridgeConnection(
     val name : String,
     private val bluetoothSocket : BluetoothSocket,
-    private val outChannel : ByteWriteChannel,
+    private val netSocket : Socket,
     private val coroutineScope : CoroutineScope
 ) : Closeable, AutoCloseable {
 
     private val log = KotlinLogging.logger("${javaClass.simpleName}[$name]")
-    private lateinit var job : Job
+    private lateinit var bluetoothToInet : Job
+    private lateinit var inetToBluetooth : Job
 
     suspend fun start() {
-        job = coroutineScope.launch {
+        bluetoothToInet = coroutineScope.launch {
+            val netOut = netSocket.openWriteChannel()
+
             var finished = false
             var received : ByteArray
-            log.info("started")
-            while(!finished) {
+            log.info("started bluetooth to inet job")
+            while(!finished && isActive) {
                 try {
                     received = bluetoothSocket.receive()
-                    log.info("received ${received.size} bytes")
-                    outChannel.writeFully(received)
+                    log.info("received ${received.size} bytes from bluetooth")
+                    netOut.writeFully(received)
                     log.info("bytes sent to net socket")
                 } catch (e : Exception) {
                     finished = true
                     log.catching(e)
                 }
             }
-            log.info("terminated")
+            log.info("terminated bluetooth to inet job")
+        }
+
+        inetToBluetooth = coroutineScope.launch {
+            val netIn = netSocket.openReadChannel()
+            var finished = false
+            var readed : Int
+            var buff : ByteBuffer = ByteBuffer.allocate(1024)
+
+            log.info("started inet to bluetooth job")
+            while(!finished && isActive) {
+                try {
+                    readed = netIn.readAvailable(buff)
+                    log.info("received ${readed} bytes from inet")
+                    bluetoothSocket.send(buff.array(), 0, readed)
+                    buff.clear()
+                    log.info("bytes sent to bluetooth")
+                } catch (e : Exception) {
+                    finished = true
+                    log.catching(e)
+                }
+            }
+            log.info("terminated inet to bluetooth job")
         }
     }
 
     override fun close() {
         runBlocking(Dispatchers.IO) {
             bluetoothSocket.close()
-            outChannel.close()
+            netSocket.close()
             withContext(Dispatchers.Default) {
-                job.join()
+                bluetoothToInet.join()
             }
         }
     }
